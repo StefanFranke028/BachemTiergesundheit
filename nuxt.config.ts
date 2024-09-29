93cca57862fb5a30be5880ff454aa7d5520a8ffb
StefanFranke-ITC <franke@itc-dortmund.de>
2024-09-29 18:39:11 +0200

    first

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     ssr: true,
+    target: 'server',
     compatibilityDate: '2024-04-03',
     devtools: {enabled: true},
     modules: ['@pinia/nuxt', 'vuetify-nuxt-module']
