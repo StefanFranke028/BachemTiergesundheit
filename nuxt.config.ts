9577e999a97d2692050b535a3bcc23f8eef77b0a
Leandro <leandro.graf@itc-studenten.de>
2024-10-07 21:37:48 +0200

    update google font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,13 +2,16 @@ import compression from 'compression';
 
 export default defineNuxtConfig({
     ssr: true,
-    serverMiddleware: [
-        compression({
-            threshold: 10240, // Mindestgröße in Bytes, ab der Dateien komprimiert werden (z.B. 10 KB)
-            level: 6,         // Kompressionslevel (zwischen 1 und 9, wobei 9 das stärkste ist)
-        })
-    ],
     target: 'server',
+    serverHandlers: [
+        {
+            route: '/api', // Stelle sicher, dass alle API-Routen die Komprimierung verwenden
+            handler: compression({
+                threshold: 10240, // Mindestgröße in Bytes, ab der Dateien komprimiert werden (z.B. 10 KB)
+                level: 6,         // Kompressionslevel (zwischen 1 und 9, wobei 9 das stärkste ist)
+            }),
+        },
+    ],
     vite: {
         define: {
             global: 'window',
