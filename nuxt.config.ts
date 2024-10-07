780754e664b5dff9f4c15c065b7a6077fc88c5ed
Leandro <leandro.graf@itc-studenten.de>
2024-10-07 21:44:12 +0200

    compression.js wer fragt ist dumm

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,6 @@
-import compression from 'compression';
-
 export default defineNuxtConfig({
     ssr: true,
     target: 'server',
-    serverHandlers: [
-        {
-            route: '/*', // Anwenden auf alle Routen
-            handler: (req, res, next) => compression()(req, res, next), // Sicherstellen, dass der Handler korrekt als Funktion definiert ist
-        },
-    ],
     vite: {
         define: {
             global: 'window',
