852f8583f94cdf0fa03fbcd9eda5bd37cc08a2ee
StefanFranke-ITC <franke@itc-dortmund.de>
2024-11-23 11:30:42 +0100

    damen page fertig

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,4 +9,15 @@ export default defineNuxtConfig({
     compatibilityDate: '2024-04-03',
     devtools: {enabled: true},
     modules: ['@pinia/nuxt', 'vuetify-nuxt-module'],
+    sitemap: {
+        hostname: 'https://maxi-escort.de', // Deine Website-URL
+        routes: async () => {
+            // Dynamische Routen, falls erforderlich
+            return [
+                '/seite1',
+                '/seite2',
+                '/dynamisch/route1',
+            ];
+        },
+    },
 });
