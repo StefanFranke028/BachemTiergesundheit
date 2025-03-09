e0341023d4a5a17d33590acaa478667f9b4fd940
Stefan.Franke.VRVoice <franke@itc-dortmund.de>
2025-03-09 21:07:54 +0100

    damen page fertig

--- pages/impressum.vue
+++ pages/impressum.vue
@@ -1,28 +1,239 @@
 <template>
-  <div v-if="desktop|| tabletHorizontal"  style="background-color: #ffffff" class="pa-5 mt-4">
-    <v-row style="width: 100% ;" class="mx-0 justify-center">
-      <v-col cols="7">
-        <v-img src="../assets/1i.jpg" cover></v-img>
-      </v-col>
-    </v-row>
+<div>
+  <div>
+    <v-row style="width: 100%;" class="d-flex justify-center ma-0 mt-10">
+      <v-col class="d-flex justify-center" cols="8">
+        <div style="width: 100%;">
+          <h2>
+          Impressum
+          </h2>
+          <p class="mt-2">
+            Mila Escort Service Frankfurt <br> <br>
+            Bitte beachten Sie, dass bei Abschluss einer Buchung der Vertrag ausschließlich mit
+            der Escort Dame zustande kommt. Mila Escort fungiert als Vermittler und bringt Sie
+            mit Ihrer Wunschbegleitung zusammen. Um die Anonymität sowohl des Kunden als
+            auch der Begleitperson zu wahren, erfolgt die bargeldlose Bezahlung an die Agentur.
+            Die Mila Escort Agentur verfügt über eine Geldempfangsvollmacht der
+            Begleitpersonen und leitet das Geld an diese weiter. Unsere Aufgabe ist lediglich die
+            Vermittlung eines passenden Termins mit der von Ihnen gewünschten Dame. Der
+            Vertragsabschluss erfolgt im Namen und auf Rechnung der Escort Dame.
+            Die Bandbreite des Services, den unsere Begleitpersonen – auch in Bad Homburg –
+            auf Reisen oder bei besonderen Anlässen bieten, richtet sich sowohl nach Ihren
+            Vorstellungen als auch nach den vor Ort geltenden Bestimmungen.
+            Länderspezifische Anordnungen und etwaige Einschränkungen werden von den
+            selbstständigen High-Class Begleitpersonen selbstverständlich beachtet.
+          </p>
+          <p class="mt-3">
+            Verantwortlich für den Inhalt dieser Website Angaben gemäß § 5 DDG
+          </p>
+          <div>
+            <p>
+              <b>
+                Mila Concepts
+              </b> <br>
+              c/o ….
+            </p>
+            <br>
 
-  </div>
-  <div v-if="mobile || tablet">
-    <v-row style="width: 100% ;" class="mx-0 justify-center">
-      <v-col cols="12">
-        <v-img src="../assets/1i.jpg" cover></v-img>
+          </div>
+          <div>
+            <p>
+              <b>
+                Steuernummer:
+              </b> <br>
+              Auf schriftliche Anfrage
+            </p>
+            <br>
+          </div>
+          <div>
+            <p>
+              <b>
+                Erreichbarkeiten:
+              </b> <br>
+              +49… <br>
+              E-Mail: …
+            </p>
+            <br>
+          </div>
+          <div>
+            <p>
+              <b>
+                Erlaubnis zum Betrieb einer Escort Agentur
+              </b> <br>
+              Der Begleit- &amp; Escort Agentur „Mila Escort“ wurde gemäß § 12 ProstSchG die
+              Erlaubnis zum Betreiben einer Escort Agentur erteilt.
+            </p>
+            <br>
+          </div>
+         <div>
+            <p>
+              <b>
+                Aufsichtsbehörde:
+              </b> <br>
+              Auf schriftliche Anfrage
+            </p>
+            <br>
+          </div>
+          <div>
+            <p>
+              <b>
+                Jugendschutzbeauftragte*r:
+              </b> <br>
+              Auf schriftliche Anfrage
+            </p>
+            <br>
+          </div>
+          <div>
+            <p>
+              <b>
+                Inhalt des Onlineangebotes
+              </b> <br>
+              Die Informationen auf dieser Website werden mit größter Sorgfalt erstellt. Dennoch
+              übernimmt der Autor keine Gewähr für die Aktualität, Richtigkeit oder Vollständigkeit
+              der bereitgestellten Inhalte. Haftungsansprüche gegen den Autor, die sich auf
+              Schäden materieller oder immaterieller Art beziehen, die durch die Nutzung oder
+              Nichtnutzung der dargebotenen Informationen verursacht werden, sind
+              ausgeschlossen, sofern kein nachweislich vorsätzliches oder grob fahrlässiges
+              Verhalten vorliegt. Alle Angebote sind unverbindlich. Der Autor behält sich das Recht
+              vor, Teile der Seiten oder das gesamte Angebot ohne vorherige Ankündigung zu
+              verändern oder zu löschen.
+            </p>
+            <br>
+          </div>
+          <div>
+            <p>
+              <b>
+                Verwendung des Begriffs „unsere“
+              </b> <br>
+              Die Verwendung des Begriffs „unsere“ in Bezug auf Damen, Escort Damen usw.
+              bezieht sich ausschließlich auf die selbstständigen Damen, mit denen wir einen
+              Werbe- und Vermittlungsvertrag haben und die nicht in einem Arbeitsverhältnis zu
+              uns stehen.
+              Die Mila Escort Agentur fungiert als Plattform für Escort Damen, die in eigener
+              Verantwortung und gewerblich selbstständig tätig sind. Diese Damen haben uns die
+              Vollmacht erteilt, in ihrem Namen zu kommunizieren und Honorare für ihre
+              Dienstleistungen anzunehmen und weiterzuleiten. Die Entscheidung über die
+              Annahme oder Ablehnung einer Buchung liegt ausschließlich bei den Damen.
+            </p>
+            <br>
+          </div>
+          <div>
+            <p>
+              <b>
+                Links zu externen Webseiten
+              </b> <br>
+              Bei direkten oder indirekten Verweisen auf externe Webseiten, die außerhalb des
+              Verantwortungsbereiches des Autors liegen, haftet der Autor nur, wenn er von den
+              Inhalten Kenntnis hatte und es ihm technisch möglich und zumutbar war, die Nutzung
+              im Falle rechtswidriger Inhalte zu verhindern. Der Autor hat zum Zeitpunkt der
+              Linksetzung keine illegalen Inhalte auf den verlinkten Seiten erkannt. Auf die
+              zukünftige Gestaltung der verlinkten Seiten hat der Autor keinen Einfluss und
+              distanziert sich daher ausdrücklich von allen Inhalten, die nach der Linksetzung
+              verändert wurden.
+            </p>
+            <br>
+          </div>
+          <div>
+            <p>
+              <b>
+                Urheber- und Kennzeichenrecht
+              </b> <br>
+              Die Inhalte dieser Webseite unterliegen dem Urheberrecht. Der Autor ist bestrebt, in
+              allen Publikationen die Urheberrechte der verwendeten Grafiken, Tondokumente und
+              Texte zu beachten. Alle genannten Marken und Warenzeichen unterliegen dem
+              jeweiligen Eigentumsrecht der Inhaber. Eine Vervielfältigung oder Verwendung
+              solcher Inhalte in anderen Publikationen ist ohne ausdrückliche Zustimmung des
+              Autors nicht gestattet.
+            </p>
+            <br>
+          </div>
+          <div>
+            <p>
+              <b>
+                Hinweis zu Bildmaterial und Grafiken
+              </b> <br>
+              Alle auf dieser Website verwendeten Bilder und Grafiken sind entweder
+              urheberrechtlich geschützt oder stammen aus lizenzierten Quellen. Die Rechte an
+              den Bildern der Escort Damen liegen bei den jeweiligen Damen. Eine
+              Vervielfältigung, Verwendung oder Bearbeitung dieser Bilder ist ohne ausdrückliche
+              schriftliche Genehmigung der jeweiligen Urheber nicht gestattet.
+              Zusätzlich unterliegen die Texte und Inhalte dieser Website dem Urheberrecht von
+              Mila Escort. Jegliche Form der Vervielfältigung, Verbreitung oder sonstigen
+              Verwendung dieser Inhalte ist ohne vorherige schriftliche Zustimmung von Mila
+              Escort untersagt.
+              Für Fragen zu den verwendeten Bildern oder zur Lizenzierung kontaktieren Sie uns
+              bitte unter: E-Mail: Support@Mila-Escort.de
+            </p>
+            <br>
+          </div>
+          <div>
+            <p>
+              <b>
+                Quellenangaben für die verwendeten Bilder
+              </b> <br>
+              https://unsplash.com/de
+            </p>
+            <br>
+          </div>
+          <div>
+            <p>
+              <b>
+                Rechtswirksamkeit dieses Haftungsausschlusses
+              </b> <br>
+              Dieser Haftungsausschluss ist als Teil des Internetangebotes zu betrachten, von dem
+              aus auf diese Seite verwiesen wurde. Sofern Teile oder einzelne Formulierungen
+              dieses Textes der geltenden Rechtslage nicht, nicht mehr oder nicht vollständig
+              entsprechen, bleiben die übrigen Teile des Dokuments in ihrem Inhalt und ihrer
+              Gültigkeit davon unberührt.
+            </p>
+            <br>
+          </div>
+          <div>
+            <p>
+              <b>
+                Ihre Privatsphäre – (Rechtliche Hinweis) <br>
+                Mila Escort – Ihre professionelle Vermittlungsagentur
+              </b> <br> <br>
+              Die Escort &amp; Vermittlungsagentur Mila Escort ist eine renommierte Internetplattform,
+              die selbstständige - freiberufliche Escort Damen aus Deutschland, Österreich und der
+              Schweiz vermittelt. Unsere Damen handeln in eigener Verantwortung und sind als
+              selbstständige freiberufliche Escort Models tätig. Die Agentur &amp; Plattform Mila Escort
+              hat zu keinem Zeitpunkt Weisungsbefugnis gegenüber den einzelnen Escort Damen.
+              Diese haben uns die Vollmacht erteilt, die Kommunikation mit Kunden zu
+              übernehmen und die Abwicklung von Termin und Date Anfragen zu koordinieren. Alle
+              Entscheidungen hinsichtlich der Annahme, Ablehnung, Verlängerung oder des
+              Abbruchs von Buchungen obliegen ausschließlich den freiberuflichen Escort Damen.
+            </p>
+            <p> <br>
+              Die Verwendung des Begriffs „unsere“ in Verbindung mit Damen, Ladys, Models und
+              Escort Damen bezieht sich ausschließlich auf die selbstständigen &amp; freiberuflichen
+              Unternehmerinnen, die über unsere Plattform Mila Escort Service vermittelt werden.
+              Es besteht kein Angestelltenverhältnis zu den Escort Damen.
+              Die Escort Models &amp; Escort Damen agieren und handeln eigenverantwortlich. Die
+              Agentur Mila hat lediglich einen Werbe- und Vermittlungsvertrag mit diesen Damen.
+            </p>
+            <p> <br>
+              © 2025 Mila Escort – Alle Rechte vorbehalten. <br>
+              High Class Escort Agentur von Mila Escort International High Class Escort Service. <br>
+
+              Die Inhalte und Bilder auf dieser Webseite sind urheberrechtlich geschützt und <br>
+              dürfen ohne ausdrückliche Genehmigung nicht kopiert, verwendet oder verbreitet <br>
+              werden.
+            </p>
+            <br>
+          </div>
+        </div>
       </v-col>
     </v-row>
   </div>
+</div>
 </template>
 <script>
-import HeaderComponent from "@/components/HeaderComponent.vue";
-import FooterComponent from "@/components/FooterComponent.vue";
+
 import {useScreenStore} from "~/stores/screen.js";
 
 export default {
   name: "KontaktView",
-  components: {FooterComponent, HeaderComponent},
   computed:{
     wide() {
       const screenStore = useScreenStore();
@@ -49,5 +260,12 @@ export default {
 </script>
 
 <style scoped>
-
+p {
+  font-family: "Aptos", sans-serif;
+  font-optical-sizing: auto;
+  font-weight: 500;
+  font-style: normal;
+  line-height: 1.3;
+  font-size: 0.8rem;
+}
 </style>
