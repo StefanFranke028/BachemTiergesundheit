fdeabc1e7472a2c39537e0535acca26c22557f1d
Stefan.Franke.VRVoice <stefan.franke@vrvoice.de>
2025-07-07 10:23:47 +0200

    titel angepasst

--- components/HeaderComponent.vue
+++ components/HeaderComponent.vue
@@ -1,79 +1,149 @@
 <template>
   <div class="header1">
-    <v-row class="pa-0" style="width: 100%; height: 100%">
-      <v-col class="pa-0 d-flex justify-center align-center" cols="2">
-        <v-img alt="Mila Escort Logo" class="" src="~/assets/LogoNew.svg"
+
+    <v-row class="justify-center mx-0 px-6 bg" style="width: 100%">
+      <v-col class="pa-0 d-flex  align-center" cols="2">
+        <v-img alt="Tier-Gesundheits logo" class="" src="~/assets/logo-weiß.png"
                style="height: 130px; width: 130px; cursor: pointer" @click="navigateTo('/')"></v-img>
       </v-col>
-
-      <v-col class="pa-0 d-flex justify-center align-center" cols="8">
-        <v-row class="d-flex justify-center">
-          <v-col class="d-flex justify-center align-center" cols="2">
-            <NuxtLink to="/vita">Unsere Vita</NuxtLink>
+      <v-col cols="10">
+        <v-row style="width: 100%">
+          <v-col class="d-flex align-center" cols="2">
+            <h5  :class="{ 'active': isPhysioRoute }" class="link text-white text-center">
+              <router-link aria-label="Chiropraktik" class="link" to="/Chiropraktik">
+                Chiropraktik
+              </router-link>
+            </h5>
+          </v-col>
+          <v-col  class="d-flex align-center" cols="2">
+            <h5  :class="{ 'active': isPhysioRoute }"
+                 class="link text-white text-center">
+              <router-link class="link" aria-label="Osteopathie"  to="/Osteopathie">
+                Osteopathie
+              </router-link>
+            </h5>
+          </v-col>
+          <v-col class="d-flex align-center" cols="2">
+            <h5  :class="{ 'active': isPhysioRoute }"
+                 class="link text-white text-center" >
+              <router-link class="link" aria-label="Physiotherapie"  to="/Physiotherapie">
+                Physiotherapie
+              </router-link>
+            </h5>
+          </v-col>
+          <v-col  class="d-flex align-center" cols="2">
+            <h5 :class="{ 'active': isPhysioRoute }" class="link text-white text-center"
+            >
+              <a class="link" aria-label="Ernährung" target="_blank"  href="https://gesundesfutterandreabachem.de/">
+                Ernährung
+              </a>
+            </h5>
+          </v-col>
+          <v-col class="pl-0" cols="3">
 
           </v-col>
-          <v-col class="d-flex justify-center  align-center" cols="2">
-            <NuxtLink to="/vision">Vision</NuxtLink>
-          </v-col>
-          <v-col class="d-flex justify-center  align-center align-center" cols="2">
-            <NuxtLink to="/highClassBegleitungen">High-Class Begleitungen</NuxtLink>
-          </v-col>
-          <v-col class="d-flex justify-center  align-center" cols="2">
-            <NuxtLink to="/casting">Casting</NuxtLink>
+          <v-col class="d-flex align-center" cols="2">
+            <h5 :class="{ 'active': isPhysioRoute }" class="link text-white text-center">
+              <router-link class="link" aria-label="Berichte"  to="/berichte">
+                Berichte
+              </router-link>
+            </h5>
 
           </v-col>
-          <v-col class="d-flex justify-center  align-center" cols="2">
-            <NuxtLink to="/getInTouch">Get in Touch</NuxtLink>
+          <v-col class="d-flex align-center" cols="2">
+            <h5 :class="{ 'active': isPhysioRoute }" class="link text-white text-center">
+              <router-link class="link" aria-label="Produkte"  to="/Produkt">
+                Produkt
+              </router-link>
+            </h5>
 
           </v-col>
-          <v-col class="d-flex justify-center  align-center" cols="2">
-            <NuxtLink to="/konditionen">Konditionen</NuxtLink>
+          <v-col class="d-flex align-center" cols="2">
+            <h5 :class="{ 'active': isPhysioRoute }" class="link text-white text-center">
+              <router-link class="link" aria-label="Seminare"  to="/Seminare">
+                Seminare
+              </router-link>
+            </h5>
 
           </v-col>
+          <v-col class="d-flex align-center" cols="2">
+            <h5  :class="{ 'active': isPhysioRoute }" class="link text-white text-center">
+              <router-link class="link" aria-label="Preise"  to="/Preise">
+                Preise
+              </router-link>
+            </h5>
 
+          </v-col>
         </v-row>
-
       </v-col>
-      <v-col class="pa-0 d-flex justify-center align-center" cols="2" @click="navigateTo('/')">
-        <div style="cursor: pointer">
-      <span style="font-size: 22px; color: #4e4e4e; ">
-          MILA
-        <b class="ml-n1" style="color: black">
-        ESCORT
-        </b>
-      </span>
-        </div>
-      </v-col>
-
     </v-row>
+
   </div>
 </template>
 
 <script>
 export default {
-  name: "header1"
+  name: "header1",
+  computed:{
+    isPhysioRoute() {
+      return this.$route.path === '/Physiotherapie' || this.$route.path === '/Osteopathie' || this.$route.path === '/Chiropraktik' || this.$route.path === '/Ernaehrung' || this.$route.path === '/berichte' || this.$route.path === '/Preise' || this.$route.path === '/Video' || this.$route.path === '/Produkt' || this.$route.path === '/Seminare';
+    }
+  }
 }
 </script>
 
 
 <style scoped>
-.v-row {
-  width: 100%;
-  padding: 0px;
-  margin: 0px
+.logo {
+  background-image: url("../assets/logo-weiß.png");
+  background-size: cover;
+  width: 248px;
+  height: 93px;
+}
+.header1{
+  background-color: black;
+}
+.logo-tablet {
+  background-image: url("../assets/logo-weiß.png");
+  background-size: cover;
+  margin-left: -70px;
+  width: 248px;
+  height: 93px;
 }
 
-.header {
-  width: 100vw;
-  height: 130px;
-  background-color: #ffffff;
-  box-shadow: 2px 2px 4px black;
+.logo1 {
+  background-image: url("../assets/Logo2.png");
+  background-size: cover;
+  width: 210px;
+  height: 93px;
 }
 
-span {
-  color: #494848;
-  font-size: 13px;
+.logo1-tablet {
+  background-image: url("../assets/Logo2.png");
+  background-size: cover;
+  margin-left: -50px;
+  width: 210px;
+  height: 93px;
+}
+
+.link {
   cursor: pointer;
+  text-decoration: none;
+  color: #0082c2 !important;
 }
 
+.active {
+  color: #0082c2 !important;
+  text-shadow: 1px 1px 1px black;
+}
+
+.link:hover {
+  color: white !important;
+}
+
+.logoscale {
+  transform: scale(0.8);
+  transform-origin: top left;
+  margin-top: -10px;
+}
 </style>
--- pages/[stadt].vue
+++ pages/[stadt].vue
@@ -1,51 +1,81 @@
 <template>
-  <div>
-    <v-img alt="schwarz-weis Hintergrundbild" class="d-flex justify-center align-center" cover
-           src="~/assets/Stadt.jpg" style="width: 100%; height: 450px;  ">
-      <div class="d-flex justify-center align-center"
-           style="height: 450px; width: 100vw; background-color: rgba(0,0,0,0.6); z-index: 9000;">
-        <h1 class="text-white text-center dm-serif">Luxusbegleitung für besondere Momente<br> in <br> {{ stadtparam }}
+  <article>
+    <!-- Header mit Bild und H1 -->
+    <v-img
+        alt="Schwarz-Weiß-Hintergrundbild"
+        class="d-flex justify-center align-center"
+        cover
+        src="~/assets/Stadt.jpg"
+        style="width: 100%; height: 450px;"
+    >
+      <div
+          class="d-flex justify-center align-center"
+          style="height: 450px; width: 100vw; background-color: rgba(0,0,0,0.6); z-index: 9000;"
+      >
+        <h1 class="text-white text-center dm-serif">
+          Luxusbegleitung für besondere Momente in {{ stadtparam }}
         </h1>
-
-
       </div>
     </v-img>
-    <div class="d-flex align-center" style="background-color: #f1f0f0; min-height: 750px">
-      <v-row class="mx-0 justify-center" style="width: 100%; min-height: 500px">
-        <v-col class="d-flex align-center py-8" cols="12" md="10" style="width: 100%; min-height: 500px">
-          <v-card style="min-height: 300px ; width: 100%">
-            <h2 class="text-center"> Unsere Damen aus {{ stadtparam }}</h2>
-            <v-divider class="mx-12"></v-divider>
+
+    <!-- Einleitungstext (optional für SEO) -->
+    <section class="text-center py-6">
+      <p style="max-width: 800px; margin: auto; font-size: 18px;">
+        Entdecken Sie unsere stilvollen Damen in {{ stadtparam }} – für niveauvolle Begegnungen und unvergessliche Erlebnisse voller Diskretion.
+      </p>
+    </section>
+
+    <!-- Stadtbeschreibung -->
+    <section style="background-color: #f1f0f0; min-height: 750px;">
+      <v-row class="mx-0 justify-center">
+        <v-col cols="12" md="10" class="d-flex align-center py-8">
+          <v-card style="min-height: 300px; width: 100%;">
+            <h2 class="text-center">Unsere Damen aus {{ stadtparam }}</h2>
+            <v-divider class="mx-12" />
             <v-card-text>
-              <h3 class="text-center">
-                {{ stadt?.überschrift }}
-                <br>
-
-              </h3>
-              <p :style="mobile ? 'font-size: 12px':'' " class="mt-3" style="text-align: justify;   "
-                 v-html="stadt?.text ">
-
-              </p>
+              <h3 class="text-center">{{ stadt?.überschrift }}</h3>
+              <div
+                  class="mt-3"
+                  style="text-align: justify;"
+                  :style="mobile ? 'font-size: 12px' : ''"
+                  v-html="stadt?.text"
+              />
             </v-card-text>
           </v-card>
         </v-col>
       </v-row>
-    </div>
-    <div class="pb-10" style="background-color: #ffffff; min-height: 400px">
-      <v-row class="mx-0 mt-4 justify-center" style="width: 100%; height: 100%">
-        <v-col v-for="dame in stadtDamen" :key="dame" class="d-flex justify-center" cols="11" md="3" xl="3">
+    </section>
+
+    <!-- Damenliste -->
+    <section class="pb-10" style="background-color: #ffffff;">
+      <v-row class="mx-0 mt-4 justify-center">
+        <v-col
+            v-for="dame in stadtDamen"
+            :key="dame.name"
+            class="d-flex justify-center"
+            cols="11"
+            md="3"
+            xl="3"
+        >
           <v-card style="width: 350px">
-            <v-img :src="dame.bilder[0].imageBase64" alt="Bilder der Damen" class="d-flex justify-center align-center" cover
-                   style="width: 100%; height: 450px;  ">
-            </v-img>
+            <v-img
+                :src="dame.bilder?.[0]?.imageBase64"
+                :alt="`Bild von ${dame.name}`"
+                class="d-flex justify-center align-center"
+                cover
+                style="width: 100%; height: 450px;"
+            />
             <v-card-title class="text-center">{{ dame.name }}</v-card-title>
-            <v-card-subtitle class="text-center">Aus {{ stadtparam }} | {{ dame.geburtsalter }}</v-card-subtitle>
+            <v-card-subtitle class="text-center">
+              Aus {{ stadtparam }} | {{ dame.geburtsalter }}
+            </v-card-subtitle>
           </v-card>
         </v-col>
       </v-row>
-    </div>
-  </div>
+    </section>
+  </article>
 </template>
+
 
 <script setup>
 
